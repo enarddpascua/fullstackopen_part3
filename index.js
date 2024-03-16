32400a9468d99e1b6007eabdbedd506319d89646
Enard <enard@Enards-MacBook-Pro.local>
2024-03-16 18:07:47 +1100

    3.13-3.14

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
+require("dotenv").config();
 const app = express();
 const morgan = require("morgan");
 const cors = require("cors");
+const Person = require("./model/Person");
 
 morgan.token("tae", (req, res) => {
   return JSON.stringify(req.body);
@@ -12,78 +14,46 @@ app.use(express.json());
 app.use(cors());
 app.use(morgan(":method :url :status :response-time ms :tae", "immediate"));
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
-}
-
 app.get("/api/persons", (request, response) => {
-  response.json(persons);
+  Person.find({}).then((person) => {
+    response.json(person);
+  });
 });
 
-app.get("/info", (request, response) => {
-  response.send(
-    `<p>Phone has info for ${persons.length} <br/> ${new Date()}</p>`
-  );
+app.get("/api/info", (request, response) => {
+  Person.countDocuments({}).then((number) => {
+    response.send(`<p>Phone has info for ${number} <br/> ${new Date()}</p>`);
+  });
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  let id = Number(request.params.id);
-  let person = persons.filter((p) => p.id === id);
-  if (person.length <= 0) {
-    response.status(404).end();
-  } else {
-    response.json(person[0]);
-  }
+  console.log(request.params.id, typeof request.params.id);
+  Person.findById(request.params.id)
+    .then((person) => {
+      response.json(person);
+    })
+    .catch((err) => {
+      response.status(404).end();
+    });
 });
 
 app.delete("/api/persons/:id", (request, response) => {
-  let id = Number(request.params.id);
-  persons = persons.filter((p) => p.id !== id);
-  response.status(204);
+  Person.findByIdAndDelete(request.params.id)
+    .then(() => {
+      response.status(204).end();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 app.post("/api/persons", (request, response) => {
-  let { name, number } = request.body;
-  let id = getRandomInt(99);
-  let validateIfNameExist = persons.filter((p) => p.name == name);
-  if (!name || !number) {
-    response.status(400).send({ message: "All fields are required" });
-  }
-
-  if (validateIfNameExist.length > 0) {
-    response.status(400).send({ message: "name must be unique" });
-    return;
-  }
-
-  let newPerson = { name, number, id };
-  persons = persons.concat(newPerson);
-  response.status(200).send(newPerson);
+  Person.create(request.body).then((res) => {
+    response.json(res).status(200).end();
+  });
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
